ed1d4f132c7cf89daf08800b8088cfc431c03424
liveworldstrike <lazodellamas12@gmail.com>
2016-01-08 13:36:08 +0100

    añadidos tantos porcientos

--- Usuario.java
+++ Usuario.java
@@ -17,6 +17,12 @@ public class Usuario
     private float ggrasasUser;
     //almacena calorias del usuario
     private float caloriasUsertotales;
+    // porciento de los gramos de proteinas del usuario
+    private float porcientoproteinasUser;
+    // porciento de los gramos de carbohidratos del usuario
+    private float porcientogcarbohidratosUser;
+    // porciento de los gramos de grasas del usuario
+    private float porcientoggrasasUser;
 
     /**
      * Constructor for objects of class Usuario
@@ -28,6 +34,9 @@ public class Usuario
         this.gcarbohidratosUser = 0;
         this.ggrasasUser = 0;
         this.caloriasUsertotales = 0;
+        this.porcientoproteinasUser = 0;
+        this.porcientogcarbohidratosUser = 0;
+        this.porcientoggrasasUser = 0;
     }
 
     /**
@@ -39,9 +48,14 @@ public class Usuario
         gcarbohidratosUser = gcarbohidratosUser +((comida.getGcarbohidratos()/100)*cantidad);
         ggrasasUser = ggrasasUser +((comida.getGgrasas()/100)*cantidad);
         caloriasUsertotales=(proteinasUser*4)+(gcarbohidratosUser*4)+(ggrasasUser*9);
-        
+
+        float totalgramos = proteinasUser + gcarbohidratosUser +  ggrasasUser;
+        porcientoproteinasUser = (proteinasUser*totalgramos)/100;
+        porcientogcarbohidratosUser = (gcarbohidratosUser*totalgramos)/100;
+        porcientoggrasasUser = (ggrasasUser*totalgramos)/100;
+
     }
-    
+
     /**
      *muestras todos los datos del usuario que ha comido
      * 
@@ -49,10 +63,10 @@ public class Usuario
     public void muestraDatos()
     {
         System.out.println("Nombre:"+user);
-        System.out.println("Gramos totales de proteinas ingeridos:"+ proteinasUser);
-        System.out.println("Gramos totales de carbohidratos ingeridos:"+ gcarbohidratosUser);
-        System.out.println("Gramos totales de grasas ingeridos:"+ ggrasasUser);
+        System.out.println("Gramos totales de proteinas ingeridos:"+ proteinasUser +"( "+ porcientoproteinasUser +"%)");
+        System.out.println("Gramos totales de carbohidratos ingeridos:"+ gcarbohidratosUser + "( " +porcientogcarbohidratosUser +"%)");
+        System.out.println("Gramos totales de grasas ingeridos:"+ ggrasasUser+"( "+porcientoggrasasUser +"%)");
         System.out.println("Calorias totales ingeridas:"+ caloriasUsertotales);
-        
+
     }
 }
